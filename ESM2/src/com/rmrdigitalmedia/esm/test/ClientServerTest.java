d7b1d212ce9b21f88bc44ae03833e68316c60222
Rich Manley-Reeve <rich@rmrdigitalmedia.co.uk>
2014-09-27 15:23:38 +0000

    mysql remote db test

--- ESM2/src/com/rmrdigitalmedia/esm/test/ClientServerTest.java
+++ ESM2/src/com/rmrdigitalmedia/esm/test/ClientServerTest.java
@@ -12,16 +12,19 @@ public class ClientServerTest {
 		try {		
 
 			/*
-			need to run this code on server
-			C:\Program Files (x86)\H2\bin>java -classpath h2-1.3.176.jar org.h2.tools.Server -tcp  -tcpAllowOthers
-
-			*/
-
-			String connStr = "jdbc:h2:tcp://192.168.1.2:9092//dbtest/ESM;";
+			for H2
+			need to run this code on server:
+			>java -classpath "C:\Program Files (x86)\H2\bin\h2-1.3.176.jar" org.h2.tools.Server -tcp  -tcpAllowOthers
 			Class.forName("org.h2.Driver");
-			Connection conn = DriverManager.getConnection(connStr, "sa", "");
+			String connStr = "jdbc:h2:tcp://192.168.1.2:9092//dbtest/ESM;";
+			*/
+			
+			// for mysql
+			Class.forName("com.mysql.jdbc.Driver");							
+			String connStr = "jdbc:mysql://www.rmrdigitalmedia.co.uk/ta6rma_esm";
+			Connection conn = DriverManager.getConnection(connStr, "ta6rma_hostuser", "theking");
 			System.out.println("Connected OK");
-			String sql = "SELECT NAME FROM VESSEL_TYPES;";
+			String sql = "SELECT NAME FROM _VESSEL_TYPES ORDER BY NAME ASC;";
 			PreparedStatement st = conn.prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 			ResultSet rs = st.executeQuery();
 			while (rs.next()) {
