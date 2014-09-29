ddb1e0dc653107f7608b8e64d0c8b37002b5d992
ta6rma <ta6rma@866917c1-198a-4af2-b025-218a88a96665>
2014-09-29 12:27:28 +0000

    tests

--- ESM2/src/com/rmrdigitalmedia/esm/test/ClientServerTest.java
+++ ESM2/src/com/rmrdigitalmedia/esm/test/ClientServerTest.java
@@ -13,16 +13,21 @@ public class ClientServerTest {
 
 			/*
 			for H2
-			need to run this code on server:
-			>java -classpath "C:\Program Files (x86)\H2\bin\h2-1.3.176.jar" org.h2.tools.Server -tcp  -tcpAllowOthers
+			//need to run this code on server:	>java -classpath "C:\Program Files (x86)\H2\bin\h2-1.3.176.jar" org.h2.tools.Server -tcp  -tcpAllowOthers
 			Class.forName("org.h2.Driver");
 			String connStr = "jdbc:h2:tcp://192.168.1.2:9092//dbtest/ESM;";
+			Connection conn = DriverManager.getConnection(connStr, "sa", "");
 			*/
-			
+
 			// for mysql
-			Class.forName("com.mysql.jdbc.Driver");							
-			String connStr = "jdbc:mysql://www.rmrdigitalmedia.co.uk/ta6rma_esm";
-			Connection conn = DriverManager.getConnection(connStr, "ta6rma_hostuser", "theking");
+			Class.forName("com.mysql.jdbc.Driver");	
+			String server = "192.168.1.30"; //www.rmrdigitalmedia.co.uk
+			String db = "ta6rma_esm";
+			String un = "ta6rma_hostuser";
+			String pw = "theking";
+			String connStr = "jdbc:mysql://"+server+"/" + db;
+			Connection conn = DriverManager.getConnection(connStr, un, pw);
+			
 			System.out.println("Connected OK");
 			String sql = "SELECT NAME FROM _VESSEL_TYPES ORDER BY NAME ASC;";
 			PreparedStatement st = conn.prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
